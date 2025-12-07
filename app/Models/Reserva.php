84481e02b733aa35345c98b479991c0d0216c1ca
Juan Marcet <juanmarcet06@gmail.com>
2025-12-07 12:45:08 -0500

    sistema de login con react y breeze

--- app/Models/Reserva.php
+++ app/Models/Reserva.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class reserva extends Model
+class Reserva extends Model
 {
     protected $table ='reservas';
     use HasFactory;
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-        collect([
-            'login' => Route::has('login'),
-            'register' => Route::has('register'),
-            'forgotPassword' => Route::has('password.request'),
-            'resetPassword' => Route::has('password.reset'),
-            'emailVerification' => Route::has('verification.notice'),
-        ])->filter(fn ($value) => $value !== false),
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
-})->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
